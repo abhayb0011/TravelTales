1974646305ce873acd62e7ae467a32fa74cd9f74
Abhay Bhardwaj <abhayb0011@gmail.com>
2024-08-28 02:24:22 +0530

    m

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post(`${baseURL}/api/v1/user/login`, {
+      const { data } = await axios.post(`https://traveltales-ghsm.onrender.com/api/v1/user/login`, {
         email: inputs.email,
         password: inputs.password,
       });
