c444d9bb6982fc5c74f6bbd8101043c516f52111
Abhay Bhardwaj <abhayb0011@gmail.com>
2024-08-29 01:06:49 +0530

    m

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -34,9 +34,26 @@ const Header = () => {
 
   return (
     <AppBar position="sticky" sx={{ backgroundColor: "#424242" }}>
-      <Toolbar sx={{ flexDirection: { xs: "column", sm: "row" }, alignItems: "center", p: 2 }}>
-        <IconButton edge="start" sx={{ p: 0, mb: { xs: 2, sm: 0 }, mr: { sm: 2 } }}>
-          <img src={logo} alt="Logo" style={{ height: "10rem", width: "auto" }} />
+      <Toolbar
+        sx={{
+          flexDirection: { xs: "column", sm: "row" },
+          alignItems: "center",
+          p: 2,
+        }}
+      >
+        <IconButton
+          edge="start"
+          sx={{
+            p: 0,
+            mb: { xs: 2, sm: 0 },
+            mr: { xs: 0, sm: 3 }, // Adjust margin-right for larger screens
+          }}
+        >
+          <img
+            src={logo}
+            alt="Logo"
+            style={{ height: "10rem", width: "auto" }}
+          />
         </IconButton>
         <Typography
           variant="h4"
@@ -52,30 +69,73 @@ const Header = () => {
           Unveiling the World's Wonders Through Captivating Blogs
         </Typography>
         {isLogin && (
-          <Box display={"flex"} flexDirection={{ xs: "column", sm: "row" }} marginLeft="auto">
-            <Button sx={{ margin: 1, color: "white" }} variant="contained" component={Link} to="/blogs">
+          <Box
+            display="flex"
+            flexDirection={{ xs: "column", sm: "row" }}
+            marginLeft="auto"
+            sx={{
+              gap: { xs: 1, sm: 2 }, // Adjust gap based on screen size
+            }}
+          >
+            <Button
+              sx={{ color: "white" }}
+              variant="contained"
+              component={Link}
+              to="/blogs"
+            >
               Blogs
             </Button>
-            <Button sx={{ margin: 1, color: "white" }} variant="contained" component={Link} to="/my-blogs">
+            <Button
+              sx={{ color: "white" }}
+              variant="contained"
+              component={Link}
+              to="/my-blogs"
+            >
               My Blogs
             </Button>
-            <Button sx={{ margin: 1, color: "white" }} variant="contained" component={Link} to="/create-blog">
+            <Button
+              sx={{ color: "white" }}
+              variant="contained"
+              component={Link}
+              to="/create-blog"
+            >
               Create Blog
             </Button>
           </Box>
         )}
-        <Box display={"flex"} flexDirection={{ xs: "column", sm: "row" }} marginLeft="auto">
+        <Box
+          display="flex"
+          flexDirection={{ xs: "column", sm: "row" }}
+          marginLeft="auto"
+          sx={{
+            gap: { xs: 1, sm: 2 }, // Adjust gap based on screen size
+          }}
+        >
           {!isLogin ? (
             <>
-              <Button sx={{ margin: 1, color: "white" }} variant="contained" component={Link} to="/login">
+              <Button
+                sx={{ color: "white" }}
+                variant="contained"
+                component={Link}
+                to="/login"
+              >
                 Login
               </Button>
-              <Button sx={{ margin: 1, color: "white" }} variant="contained" component={Link} to="/register">
+              <Button
+                sx={{ color: "white" }}
+                variant="contained"
+                component={Link}
+                to="/register"
+              >
                 Register
               </Button>
             </>
           ) : (
-            <Button onClick={handleLogout} variant="contained" sx={{ margin: 1, color: "white" }}>
+            <Button
+              onClick={handleLogout}
+              variant="contained"
+              sx={{ color: "white" }}
+            >
               Logout
             </Button>
           )}
