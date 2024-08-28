901de902bbece4d08f26bdad00729b32aa3983c1
Abhay Bhardwaj <abhayb0011@gmail.com>
2024-08-29 03:58:30 +0530

    making resonsive

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -101,7 +101,7 @@ const Header = () => {
             <img
               src={logo}
               alt="Logo"
-              style={{ height: "7rem", width: "auto" }}
+              style={{ height: "5rem", width: "auto" }}
             />
           </IconButton>
           <Typography
