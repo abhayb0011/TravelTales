2cd70cf1be380cfdf361635b7b74f765d3f25033
Abhay Bhardwaj <abhayb0011@gmail.com>
2024-08-29 03:12:37 +0530

    making responsive

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -96,7 +96,7 @@ const Header = () => {
             p: 2,
             justifyContent: "space-between",
             gap: { xs: 8, sm: 4 },
-            width: 'auto'
+            width: '100%'
           }}
         >
           <IconButton edge="start" sx={{ p: 0 }}>
