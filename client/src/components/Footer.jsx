136d980e0226c5de55dad49d41aadda835e93c0c
Abhay Bhardwaj <abhayb0011@gmail.com>
2024-08-29 01:02:30 +0530

    m

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,38 +2,22 @@ import React from "react";
 import { Box, AppBar, Toolbar, Button, IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
 import logo from "../assets/logoTravelTales.png";
-import "./Footer.css";
 
 const Footer = () => {
   return (
-    <AppBar position="relative" className="footerAppBar">
-      <Toolbar className="footerToolbar">
-        <IconButton edge="start" className="footerLogoButton">
-          <img src={logo} alt="Logo" className="footerLogo" />
+    <AppBar position="relative" sx={{ backgroundColor: "#424242", bottom: 0 }}>
+      <Toolbar sx={{ flexDirection: { xs: "column", sm: "row" }, alignItems: "center", justifyContent: "space-between", p: { xs: 2, sm: 3 } }}>
+        <IconButton edge="start" sx={{ p: 0, mb: { xs: 2, sm: 0 } }}>
+          <img src={logo} alt="Logo" style={{ height: "7rem", width: "auto" }} />
         </IconButton>
-        <Box className="footerLinks">
-          <Button
-            sx={{ color: "white" }}
-            variant="contained"
-            component={Link}
-            to="/contact"
-          >
+        <Box sx={{ display: "flex", flexDirection: { xs: "column", sm: "row" }, alignItems: "center", width: { xs: "100%", sm: "auto" } }}>
+          <Button sx={{ color: "white", mb: { xs: 1, sm: 0 }, mr: { sm: 2 } }} variant="contained" component={Link} to="/contact">
             Contact us
           </Button>
-          <Button
-            sx={{ color: "white" }}
-            variant="contained"
-            component={Link}
-            to="/terms"
-          >
+          <Button sx={{ color: "white", mb: { xs: 1, sm: 0 }, mr: { sm: 2 } }} variant="contained" component={Link} to="/terms">
             Terms and Conditions
           </Button>
-          <Button
-            sx={{ color: "white" }}
-            variant="contained"
-            component={Link}
-            to="/help"
-          >
+          <Button sx={{ color: "white" }} variant="contained" component={Link} to="/help">
             Help
           </Button>
         </Box>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,22 +7,19 @@ import {
   Button,
   Typography,
   IconButton,
-  Collapse,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { authActions } from "../redux/store";
 import toast from "react-hot-toast";
-import MenuIcon from "@mui/icons-material/Menu";
 import logo from "../assets/logoTravelTales.png";
-import "./Header.css";
 
 const Header = () => {
   const isLogin =
     useSelector((state) => state.isLogin) || localStorage.getItem("userId");
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [value, setValue] = useState(null);
 
   const handleLogout = () => {
     try {
@@ -35,140 +32,56 @@ const Header = () => {
     }
   };
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
-    <>
-      <AppBar position="sticky" className="appBar">
-        <Toolbar className="toolbar">
-          <IconButton edge="start" className="logoButton" component={Link} to="/">
-            <img src={logo} alt="Logo" className="logo" />
-          </IconButton>
-          <Typography variant="h4" className="headerTitle">
-            Unveiling the World's Wonders Through Captivating Blogs
-          </Typography>
-          <Box className="navLinks">
-            {isLogin && (
-              <>
-                <Button
-                  sx={{ margin: 1, color: "white" }}
-                  variant="contained"
-                  component={Link}
-                  to="/blogs"
-                >
-                  Blogs
-                </Button>
-                <Button
-                  sx={{ margin: 1, color: "white" }}
-                  variant="contained"
-                  component={Link}
-                  to="/my-blogs"
-                >
-                  My Blogs
-                </Button>
-                <Button
-                  sx={{ margin: 1, color: "white" }}
-                  variant="contained"
-                  component={Link}
-                  to="/create-blog"
-                >
-                  Create Blog
-                </Button>
-              </>
-            )}
-            {!isLogin && (
-              <>
-                <Button
-                  sx={{ margin: 1, color: "white" }}
-                  variant="contained"
-                  component={Link}
-                  to="/login"
-                >
-                  Login
-                </Button>
-                <Button
-                  sx={{ margin: 1, color: "white" }}
-                  variant="contained"
-                  component={Link}
-                  to="/register"
-                >
-                  Register
-                </Button>
-              </>
-            )}
+    <AppBar position="sticky" sx={{ backgroundColor: "#424242" }}>
+      <Toolbar sx={{ flexDirection: { xs: "column", sm: "row" }, alignItems: "center", p: 2 }}>
+        <IconButton edge="start" sx={{ p: 0, mb: { xs: 2, sm: 0 }, mr: { sm: 2 } }}>
+          <img src={logo} alt="Logo" style={{ height: "10rem", width: "auto" }} />
+        </IconButton>
+        <Typography
+          variant="h4"
+          sx={{
+            flexGrow: 1,
+            fontFamily: "Dancing Script, Arial",
+            fontWeight: 700,
+            fontSize: { xs: 24, sm: 32 },
+            textAlign: { xs: "center", sm: "left" },
+            mb: { xs: 2, sm: 0 },
+          }}
+        >
+          Unveiling the World's Wonders Through Captivating Blogs
+        </Typography>
+        {isLogin && (
+          <Box display={"flex"} flexDirection={{ xs: "column", sm: "row" }} marginLeft="auto">
+            <Button sx={{ margin: 1, color: "white" }} variant="contained" component={Link} to="/blogs">
+              Blogs
+            </Button>
+            <Button sx={{ margin: 1, color: "white" }} variant="contained" component={Link} to="/my-blogs">
+              My Blogs
+            </Button>
+            <Button sx={{ margin: 1, color: "white" }} variant="contained" component={Link} to="/create-blog">
+              Create Blog
+            </Button>
           </Box>
-          <IconButton
-            edge="end"
-            color="inherit"
-            onClick={toggleMenu}
-            className="menuIcon"
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-        <Collapse in={menuOpen} timeout="auto" unmountOnExit className="collapseMenu">
-          <Box className="collapsedNav">
-            {isLogin && (
-              <>
-                <Button
-                  sx={{ margin: 1, color: "white" }}
-                  variant="contained"
-                  component={Link}
-                  to="/blogs"
-                >
-                  Blogs
-                </Button>
-                <Button
-                  sx={{ margin: 1, color: "white" }}
-                  variant="contained"
-                  component={Link}
-                  to="/my-blogs"
-                >
-                  My Blogs
-                </Button>
-                <Button
-                  sx={{ margin: 1, color: "white" }}
-                  variant="contained"
-                  component={Link}
-                  to="/create-blog"
-                >
-                  Create Blog
-                </Button>
-                <Button
-                  onClick={handleLogout}
-                  variant="contained"
-                  sx={{ margin: 1, color: "white" }}
-                >
-                  Logout
-                </Button>
-              </>
-            )}
-            {!isLogin && (
-              <>
-                <Button
-                  sx={{ margin: 1, color: "white" }}
-                  variant="contained"
-                  component={Link}
-                  to="/login"
-                >
-                  Login
-                </Button>
-                <Button
-                  sx={{ margin: 1, color: "white" }}
-                  variant="contained"
-                  component={Link}
-                  to="/register"
-                >
-                  Register
-                </Button>
-              </>
-            )}
-          </Box>
-        </Collapse>
-      </AppBar>
-    </>
+        )}
+        <Box display={"flex"} flexDirection={{ xs: "column", sm: "row" }} marginLeft="auto">
+          {!isLogin ? (
+            <>
+              <Button sx={{ margin: 1, color: "white" }} variant="contained" component={Link} to="/login">
+                Login
+              </Button>
+              <Button sx={{ margin: 1, color: "white" }} variant="contained" component={Link} to="/register">
+                Register
+              </Button>
+            </>
+          ) : (
+            <Button onClick={handleLogout} variant="contained" sx={{ margin: 1, color: "white" }}>
+              Logout
+            </Button>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
